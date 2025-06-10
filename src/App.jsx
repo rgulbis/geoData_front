8d4db1fc676048c7151bd8fedab190c48b8e52b6
rgulbis <rgubis41@gmail.com>
2025-06-10 20:03:00 +0300

    aizmirsu loding

--- src/App.jsx
+++ src/App.jsx
@@ -3,21 +3,27 @@ import './App.css'
 
 function App() {
 const[countries, setCountries] = useState([]);
+const[loading, setLoading] = useState(true);
 useEffect(() => {
     async function fetchCountries() {
         const response = await fetch("http://127.0.0.1:8000/api/countries");
         const countriesAPI = await response.json();
         setCountries(countriesAPI);
+        setLoading(false);
     }
     fetchCountries();
 }, []);
   return (
     <>
-    <ul>
-    {countries.map((country) =>
-        <li key={country.id}>{country.name}</li>
-    )}
-    </ul>
+    {loading ? (
+                <h1>LODING..</h1>
+            ) : (
+                <ul>
+                {countries.map((country) =>
+                    <li key={country.id}>{country.name}</li>
+                )}
+                </ul>
+            )}
     </>
   )
 }
